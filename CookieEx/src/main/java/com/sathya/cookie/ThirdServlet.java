2ac30937b2e150d1c577cd236992315bb05ab017
prasanna2662 <132125820+prasanna2662@users.noreply.github.com>
2023-12-30 10:45:36 +0530

    Add files via upload

---
+++ CookieEx/src/main/java/com/sathya/cookie/ThirdServlet.java
@@ -0,0 +1,36 @@
+package com.sathya.cookie;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ThirdServlet")
+public class ThirdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Cookie[] cookies = request.getCookies();		
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("User name..."+cookies[0].getValue());
+		writer.println("<br>");
+		writer.println("User Age..."+cookies[1].getValue());
+		writer.println("<br>");
+		writer.println("User Qualifation..."+cookies[2].getValue());
+		writer.println("<br>");
+		writer.println("User Designation..."+cookies[3].getValue());
+		writer.println("<br>");
+		writer.println("User mobile..."+request.getParameter("mobile"));
+		writer.println("<br>");
+		writer.println("User Email..."+request.getParameter("email"));	
+	}
+
+}
