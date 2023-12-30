a6505f3b8794450cfc7a3c8e214211ff2f0ffc81
prasanna2662 <132125820+prasanna2662@users.noreply.github.com>
2023-12-30 10:30:38 +0530

    Add files via upload

---
+++ FullNameApp/src/main/java/com/sathya/fullnameservlet/fullnameservlet.java
@@ -0,0 +1,28 @@
+package com.sathya.fullnameservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class fullnameservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String FirstName=request.getParameter("FirstName");
+		String LastName=request.getParameter("lastName");
+		String FullName;
+		FullName=FirstName+" " +LastName;
+		response.setContentType("text/plain");
+		PrintWriter writer=response.getWriter();
+		writer.println("your full name "  +FullName);
+		
+		
+	}
+
+}
